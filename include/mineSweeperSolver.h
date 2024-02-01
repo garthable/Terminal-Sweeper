956aa1558c4f0274e3fb2d6b9bf091c5f3e9140f
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2024-02-01 12:11:46 -0700

    Messing around with includes

--- include/mineSweeperSolver.h
+++ include/mineSweeperSolver.h
@@ -6,7 +6,6 @@
 #ifndef MINESWEEPERSOLVER
 #define MINESWEEPERSOLVER
 
-#include "mineSweeper.h"
 #include "mineSweeperSolutionFinder.h"
 
 struct coordinate
@@ -18,30 +17,6 @@ struct coordinate
     {
         x = _x;
         y = _y;
-    }
-};
-
-struct solverTile
-{
-    std::vector<solverTile*> adjSolverTiles;
-    std::array<float, 9u> probabilityOfHavingNAdjBombs;
-    std::vector<bool> bombBackTracking;
-    float bombProbability;
-    uint16_t x;
-    uint16_t y;
-    uint16_t adjBombsAmount;
-    uint8_t solverTileState;
-
-    bool visited;
-
-    solverTile(const uint16_t& _x, const uint16_t& _y)
-    {
-        x = _x;
-        y = _y;
-        adjBombsAmount = 0u;
-        visited = false;
-        solverTileState = 0u;
-        bombProbability = 0.0;
     }
 };
 
