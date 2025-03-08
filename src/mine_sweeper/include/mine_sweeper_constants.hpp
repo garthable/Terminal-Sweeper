9e02917791c5276e7b8d45440b96b98954abc8c8
Dalton Prokosch <ddjjpp331@gmail.com>
2025-03-08 12:01:05 -0700

    added comments

--- src/mine_sweeper/include/mine_sweeper_constants.hpp
+++ src/mine_sweeper/include/mine_sweeper_constants.hpp
@@ -13,14 +13,50 @@
  */
 #define MSWP_MAX_TILES MSWP_MAX_WIDTH*MSWP_MAX_HEIGHT
 
+/**
+ * @brief Width of expert game of minesweeper
+ * 
+ */
 #define MSWP_EXPERT_WIDTH 30
+/**
+ * @brief Height of expert game of minesweeper
+ * 
+ */
 #define MSWP_EXPERT_HEIGHT 16
+/**
+ * @brief Bomb count of expert game of minesweeper
+ * 
+ */
 #define MSWP_EXPERT_BOMB_COUNT 99
 
+/**
+ * @brief Width of intermediate game of minesweeper
+ * 
+ */
 #define MSWP_INTERMEDIATE_WIDTH 16
+/**
+ * @brief Height of intermediate game of minesweeper
+ * 
+ */
 #define MSWP_INTERMEDIATE_HEIGHT 16
+/**
+ * @brief Bomb count of intermediate game of minesweeper
+ * 
+ */
 #define MSWP_INTERMEDIATE_BOMB_COUNT 40
 
+/**
+ * @brief Width of beginner game of minesweeper
+ * 
+ */
 #define MSWP_BEGINNER_WIDTH 9
+/**
+ * @brief Height of beginner game of minesweeper
+ * 
+ */
 #define MSWP_BEGINNER_HEIGHT 9
+/**
+ * @brief Bomb count of beginner game of minesweeper
+ * 
+ */
 #define MSWP_BEGINNER_BOMB_COUNT 10
