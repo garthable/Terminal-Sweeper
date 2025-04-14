cf66e3c13998f93762441ecc1d5cc687c943a1c8
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-04-14 10:40:42 -0600

    commented out update funcs

--- test/mine_sweeper_solver_tests/mine_sweeper_solver_test.cpp
+++ test/mine_sweeper_solver_tests/mine_sweeper_solver_test.cpp
@@ -120,237 +120,237 @@ bool compareArray(const std::array<mswp::BoardIndex, 8> buffer, const std::initi
     return true;
 }
 
-TEST(Update, 0)
-{
-    using namespace mswptileconsts;
-    using namespace slvrtileconsts;
+// TEST(Update, 0)
+// {
+//     using namespace mswptileconsts;
+//     using namespace slvrtileconsts;
 
-    mswp::MineSweeper board(5, 
-    {
-        H0, H0, H0, H0, H0,
-        H0, H0, H0, H0, H0,
-        H0, H0, H0, H0, H0,
-        H0, H0, H0, H0, H0
-    });
+//     mswp::MineSweeper board(5, 
+//     {
+//         H0, H0, H0, H0, H0,
+//         H0, H0, H0, H0, H0,
+//         H0, H0, H0, H0, H0,
+//         H0, H0, H0, H0, H0
+//     });
 
-    slvr::MineSweeperSolver mineSweeperSolver(board);
+//     slvr::MineSweeperSolver mineSweeperSolver(board);
 
-    for (mswp::BoardIndex i = 0; i < board.size(); i++)
-    {
-        ASSERT_EQ(mineSweeperSolver[i].adjBombs, -1);
-        ASSERT_EQ(mineSweeperSolver[i].adjUnknowns, -1);
-        ASSERT_NE(mineSweeperSolver[i].bombProb, mineSweeperSolver[i].bombProb);
-    }
+//     for (mswp::BoardIndex i = 0; i < board.size(); i++)
+//     {
+//         ASSERT_EQ(mineSweeperSolver[i].adjBombs, -1);
+//         ASSERT_EQ(mineSweeperSolver[i].adjUnknowns, -1);
+//         ASSERT_NE(mineSweeperSolver[i].bombProb, mineSweeperSolver[i].bombProb);
+//     }
 
-    // Test for unknown and bomb counts
-    // Test for bomb percentages
+//     // Test for unknown and bomb counts
+//     // Test for bomb percentages
 
-    board.click(0, 0);
-    mineSweeperSolver.update(board.tileString());
+//     board.click(0, 0);
+//     mineSweeperSolver.update(board.tileString());
 
-    ASSERT_TRUE(compareArray(mineSweeperSolver.tilesWithAdjBombBuffer(), 
-    {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
-    }));
+//     ASSERT_TRUE(compareArray(mineSweeperSolver.tilesWithAdjBombBuffer(), 
+//     {
+//         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
+//     }));
     
-    for (mswp::BoardIndex i = 0; i < board.size(); i++)
-    {
-        ASSERT_EQ(mineSweeperSolver[i].adjBombs, 0);
-        ASSERT_EQ(mineSweeperSolver[i].bombProb, 0);
-        ASSERT_EQ(mineSweeperSolver[i].adjUnknowns, 0);
-    }
-}
+//     for (mswp::BoardIndex i = 0; i < board.size(); i++)
+//     {
+//         ASSERT_EQ(mineSweeperSolver[i].adjBombs, 0);
+//         ASSERT_EQ(mineSweeperSolver[i].bombProb, 0);
+//         ASSERT_EQ(mineSweeperSolver[i].adjUnknowns, 0);
+//     }
+// }
 
-TEST(Update, 1)
-{
-    using namespace mswptileconsts;
-    using namespace slvrtileconsts;
+// TEST(Update, 1)
+// {
+//     using namespace mswptileconsts;
+//     using namespace slvrtileconsts;
 
-    mswp::MineSweeper board(5, 
-    {
-        H0, H2, B1, H2, H0,
-        H2, H5, B2, H3, H0,
-        B1, B2, B2, H2, H0,
-        H2, H3, H2, H1, H0
-    });
+//     mswp::MineSweeper board(5, 
+//     {
+//         H0, H2, B1, H2, H0,
+//         H2, H5, B2, H3, H0,
+//         B1, B2, B2, H2, H0,
+//         H2, H3, H2, H1, H0
+//     });
 
-    slvr::MineSweeperSolver mineSweeperSolver(board);
+//     slvr::MineSweeperSolver mineSweeperSolver(board);
 
-    for (mswp::BoardIndex i = 0; i < board.size(); i++)
-    {
-        ASSERT_EQ(mineSweeperSolver[i].adjBombs, -1);
-        ASSERT_EQ(mineSweeperSolver[i].adjUnknowns, -1);
-        ASSERT_NE(mineSweeperSolver[i].bombProb, mineSweeperSolver[i].bombProb);
-    }
+//     for (mswp::BoardIndex i = 0; i < board.size(); i++)
+//     {
+//         ASSERT_EQ(mineSweeperSolver[i].adjBombs, -1);
+//         ASSERT_EQ(mineSweeperSolver[i].adjUnknowns, -1);
+//         ASSERT_NE(mineSweeperSolver[i].bombProb, mineSweeperSolver[i].bombProb);
+//     }
 
-    // Test for unknown and bomb counts
-    // Test for bomb percentages
+//     // Test for unknown and bomb counts
+//     // Test for bomb percentages
 
-    board.click(0, 0);
-    mineSweeperSolver.update(board.tileString());
+//     board.click(0, 0);
+//     mineSweeperSolver.update(board.tileString());
 
-    ASSERT_TRUE(compareArray(mineSweeperSolver.tilesWithAdjBombBuffer(), 
-    {
-        0, 1, 5, 6
-    }));
+//     ASSERT_TRUE(compareArray(mineSweeperSolver.tilesWithAdjBombBuffer(), 
+//     {
+//         0, 1, 5, 6
+//     }));
 
-    slvr::SolverInitList solverInitList =
-    {
-        B0H0, B2H2, HIDD, HIDD, HIDD,
-        B2H2, B5H5, HIDD, HIDD, HIDD,
-        HIDD, HIDD, HIDD, HIDD, HIDD,
-        HIDD, HIDD, HIDD, HIDD, HIDD
-    };
+//     slvr::SolverInitList solverInitList =
+//     {
+//         B0H0, B2H2, HIDD, HIDD, HIDD,
+//         B2H2, B5H5, HIDD, HIDD, HIDD,
+//         HIDD, HIDD, HIDD, HIDD, HIDD,
+//         HIDD, HIDD, HIDD, HIDD, HIDD
+//     };
     
-    ASSERT_EQ(mineSweeperSolver, solverInitList);
+//     ASSERT_EQ(mineSweeperSolver, solverInitList);
 
-    // Testing repeat input
+//     // Testing repeat input
 
-    board.click(0, 0);
-    mineSweeperSolver.update(board.tileString());
+//     board.click(0, 0);
+//     mineSweeperSolver.update(board.tileString());
 
-    slvr::SolverInitList solverInitListRepeat =
-    {
-        B0H0, B2H2, HIDD, HIDD, HIDD,
-        B2H2, B5H5, HIDD, HIDD, HIDD,
-        HIDD, HIDD, HIDD, HIDD, HIDD,
-        HIDD, HIDD, HIDD, HIDD, HIDD
-    };
+//     slvr::SolverInitList solverInitListRepeat =
+//     {
+//         B0H0, B2H2, HIDD, HIDD, HIDD,
+//         B2H2, B5H5, HIDD, HIDD, HIDD,
+//         HIDD, HIDD, HIDD, HIDD, HIDD,
+//         HIDD, HIDD, HIDD, HIDD, HIDD
+//     };
     
-    ASSERT_EQ(mineSweeperSolver, solverInitListRepeat);
+//     ASSERT_EQ(mineSweeperSolver, solverInitListRepeat);
 
-    // Testing otherside click
+//     // Testing otherside click
 
-    board.click(4, 3);
-    mineSweeperSolver.update(board.tileString());
+//     board.click(4, 3);
+//     mineSweeperSolver.update(board.tileString());
 
-    ASSERT_TRUE(compareArray(mineSweeperSolver.tilesWithAdjBombBuffer(), 
-    {
-        3, 4, 8, 9, 13, 14, 18, 19
-    }));
+//     ASSERT_TRUE(compareArray(mineSweeperSolver.tilesWithAdjBombBuffer(), 
+//     {
+//         3, 4, 8, 9, 13, 14, 18, 19
+//     }));
 
-    slvr::SolverInitList solverInitListOtherSide =
-    {
-        B0H0, B2H2, HIDD, B2H2, B0H0,
-        B2H2, B5H5, HIDD, B3H3, B0H0,
-        HIDD, HIDD, HIDD, B2H2, B0H0,
-        HIDD, HIDD, HIDD, B1H1, B0H0
-    };
+//     slvr::SolverInitList solverInitListOtherSide =
+//     {
+//         B0H0, B2H2, HIDD, B2H2, B0H0,
+//         B2H2, B5H5, HIDD, B3H3, B0H0,
+//         HIDD, HIDD, HIDD, B2H2, B0H0,
+//         HIDD, HIDD, HIDD, B1H1, B0H0
+//     };
     
-    ASSERT_EQ(mineSweeperSolver, solverInitListOtherSide);
+//     ASSERT_EQ(mineSweeperSolver, solverInitListOtherSide);
 
-    // Testing flag
+//     // Testing flag
 
-    board.flag(2, 0);
-    board.flag(2, 1);
-    board.flag(2, 2);
-    board.flag(1, 2);
-    board.flag(0, 2);
-    mineSweeperSolver.update(board.tileString());
+//     board.flag(2, 0);
+//     board.flag(2, 1);
+//     board.flag(2, 2);
+//     board.flag(1, 2);
+//     board.flag(0, 2);
+//     mineSweeperSolver.update(board.tileString());
 
-    ASSERT_TRUE(compareArray(mineSweeperSolver.tilesWithAdjBombBuffer(), 
-    {
-        1, 3, 5, 6, 8, 13, 18
-    }));
+//     ASSERT_TRUE(compareArray(mineSweeperSolver.tilesWithAdjBombBuffer(), 
+//     {
+//         1, 3, 5, 6, 8, 13, 18
+//     }));
 
-    slvr::SolverInitList solverInitListFlag =
-    {
-        B0H0, B0H0, BOMB, B0H0, B0H0,
-        B0H0, B0H0, BOMB, B0H0, B0H0,
-        BOMB, BOMB, BOMB, B0H1, B0H0,
-        HIDD, HIDD, HIDD, B0H1, B0H0
-    };
+//     slvr::SolverInitList solverInitListFlag =
+//     {
+//         B0H0, B0H0, BOMB, B0H0, B0H0,
+//         B0H0, B0H0, BOMB, B0H0, B0H0,
+//         BOMB, BOMB, BOMB, B0H1, B0H0,
+//         HIDD, HIDD, HIDD, B0H1, B0H0
+//     };
     
-    ASSERT_EQ(mineSweeperSolver, solverInitListFlag);
-}
+//     ASSERT_EQ(mineSweeperSolver, solverInitListFlag);
+// }
 
-TEST(Update, 2)
-{
-    using namespace mswptileconsts;
-    using namespace slvrtileconsts;
+// TEST(Update, 2)
+// {
+//     using namespace mswptileconsts;
+//     using namespace slvrtileconsts;
 
-    mswp::MineSweeper board(5, 
-    {
-        H0, H2, B1, H2, H0,
-        H2, H5, B2, H3, H0,
-        B1, B2, B2, H2, H0,
-        H2, H3, H2, H1, H0
-    });
+//     mswp::MineSweeper board(5, 
+//     {
+//         H0, H2, B1, H2, H0,
+//         H2, H5, B2, H3, H0,
+//         B1, B2, B2, H2, H0,
+//         H2, H3, H2, H1, H0
+//     });
 
-    slvr::MineSweeperSolver mineSweeperSolver(board);
+//     slvr::MineSweeperSolver mineSweeperSolver(board);
 
-    for (mswp::BoardIndex i = 0; i < board.size(); i++)
-    {
-        ASSERT_EQ(mineSweeperSolver[i].adjBombs, -1);
-        ASSERT_EQ(mineSweeperSolver[i].adjUnknowns, -1);
-        ASSERT_NE(mineSweeperSolver[i].bombProb, mineSweeperSolver[i].bombProb);
-    }
+//     for (mswp::BoardIndex i = 0; i < board.size(); i++)
+//     {
+//         ASSERT_EQ(mineSweeperSolver[i].adjBombs, -1);
+//         ASSERT_EQ(mineSweeperSolver[i].adjUnknowns, -1);
+//         ASSERT_NE(mineSweeperSolver[i].bombProb, mineSweeperSolver[i].bombProb);
+//     }
 
-    // Test for unknown and bomb counts
-    // Test for bomb percentages
+//     // Test for unknown and bomb counts
+//     // Test for bomb percentages
 
-    board.click(0, 0);
-    mineSweeperSolver.update(board.tileString());
+//     board.click(0, 0);
+//     mineSweeperSolver.update(board.tileString());
 
-    ASSERT_TRUE(compareArray(mineSweeperSolver.tilesWithAdjBombBuffer(), 
-    {
-        0, 1, 5, 6
-    }));
+//     ASSERT_TRUE(compareArray(mineSweeperSolver.tilesWithAdjBombBuffer(), 
+//     {
+//         0, 1, 5, 6
+//     }));
 
-    slvr::SolverInitList solverInitList =
-    {
-        B0H0, B2H2, HIDD, HIDD, HIDD,
-        B2H2, B5H5, HIDD, HIDD, HIDD,
-        HIDD, HIDD, HIDD, HIDD, HIDD,
-        HIDD, HIDD, HIDD, HIDD, HIDD
-    };
+//     slvr::SolverInitList solverInitList =
+//     {
+//         B0H0, B2H2, HIDD, HIDD, HIDD,
+//         B2H2, B5H5, HIDD, HIDD, HIDD,
+//         HIDD, HIDD, HIDD, HIDD, HIDD,
+//         HIDD, HIDD, HIDD, HIDD, HIDD
+//     };
     
-    ASSERT_EQ(mineSweeperSolver, solverInitList);
+//     ASSERT_EQ(mineSweeperSolver, solverInitList);
 
-    // Testing flag
+//     // Testing flag
 
-    board.flag(2, 0);
-    board.flag(2, 1);
-    board.flag(2, 2);
-    board.flag(1, 2);
-    board.flag(0, 2);
-    mineSweeperSolver.update(board.tileString());
+//     board.flag(2, 0);
+//     board.flag(2, 1);
+//     board.flag(2, 2);
+//     board.flag(1, 2);
+//     board.flag(0, 2);
+//     mineSweeperSolver.update(board.tileString());
 
-    ASSERT_TRUE(compareArray(mineSweeperSolver.tilesWithAdjBombBuffer(), 
-    {
-        1, 5, 6
-    }));
+//     ASSERT_TRUE(compareArray(mineSweeperSolver.tilesWithAdjBombBuffer(), 
+//     {
+//         1, 5, 6
+//     }));
 
-    slvr::SolverInitList solverInitListFlag =
-    {
-        B0H0, B0H0, BOMB, HIDD, HIDD,
-        B0H0, B0H0, BOMB, HIDD, HIDD,
-        BOMB, BOMB, BOMB, HIDD, HIDD,
-        HIDD, HIDD, HIDD, HIDD, HIDD
-    };
+//     slvr::SolverInitList solverInitListFlag =
+//     {
+//         B0H0, B0H0, BOMB, HIDD, HIDD,
+//         B0H0, B0H0, BOMB, HIDD, HIDD,
+//         BOMB, BOMB, BOMB, HIDD, HIDD,
+//         HIDD, HIDD, HIDD, HIDD, HIDD
+//     };
     
-    ASSERT_EQ(mineSweeperSolver, solverInitListFlag);
+//     ASSERT_EQ(mineSweeperSolver, solverInitListFlag);
 
 
-    // Testing otherside click
+//     // Testing otherside click
 
-    board.click(4, 3);
-    mineSweeperSolver.update(board.tileString());
-    ASSERT_TRUE(compareArray(mineSweeperSolver.tilesWithAdjBombBuffer(), 
-    {
-        3, 4, 8, 9, 13, 14, 18, 19
-    }));
+//     board.click(4, 3);
+//     mineSweeperSolver.update(board.tileString());
+//     ASSERT_TRUE(compareArray(mineSweeperSolver.tilesWithAdjBombBuffer(), 
+//     {
+//         3, 4, 8, 9, 13, 14, 18, 19
+//     }));
 
-    slvr::SolverInitList solverInitListOtherSide =
-    {
-        B0H0, B0H0, BOMB, B0H0, B0H0,
-        B0H0, B0H0, BOMB, B0H0, B0H0,
-        BOMB, BOMB, BOMB, B0H1, B0H0,
-        HIDD, HIDD, HIDD, B0H1, B0H0
-    };
+//     slvr::SolverInitList solverInitListOtherSide =
+//     {
+//         B0H0, B0H0, BOMB, B0H0, B0H0,
+//         B0H0, B0H0, BOMB, B0H0, B0H0,
+//         BOMB, BOMB, BOMB, B0H1, B0H0,
+//         HIDD, HIDD, HIDD, B0H1, B0H0
+//     };
     
-    ASSERT_EQ(mineSweeperSolver, solverInitListOtherSide);
-}
+//     ASSERT_EQ(mineSweeperSolver, solverInitListOtherSide);
+// }
 
 TEST(LazySolver, 0)
 {
